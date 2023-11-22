39e5fb0736dfe5c72fb84487894e139ca97f37c2
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-11-22 09:36:03 +0700

    route to save temporary data sublocation

--- routes/sublocation.js
+++ routes/sublocation.js
@@ -7,5 +7,6 @@ const SublocationController = require('../app/Controller/SublocationController')
 router.get('/', [authenticate], SublocationController.index);
 router.post('/', [authenticate], SublocationController.store);
 router.patch('/:losc_id/capacity', [authenticate], SublocationController.inputCapacity)
+router.post('/temporary-sublocation', [authenticate], SublocationController.inputTemporary)
 
 module.exports = router
