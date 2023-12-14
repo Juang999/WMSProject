afe3475839eb82a953de4c270dc251e499a76027
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2023-12-14 10:42:29 +0700

    add new feature to post data

--- app/Controller/PickingListController.js
+++ app/Controller/PickingListController.js
@@ -2,12 +2,14 @@
 const {v4: uuidv4}  = require('uuid')
 const moment = require('moment')
 const {Auth} = require('../../helper/helper')
+const {Op} = require('sequelize')
 
 // model
 const {
     SoMstr, SodDet,
     EnMstr, InvcdDet,
     Sequelize, PtMstr, 
+    LocsMstr, sequelize,
     PcklsMstr, PcklsdDet
 } = require('../../models')
 
@@ -15,6 +17,7 @@ class PickingListController {
     getDataSalesOrder (req, res) {
         SoMstr.findOne({
             attributes: [
+                'so_oid',
                 'so_code',
                 'so_add_by',
                 [Sequelize.literal(`TO_CHAR(so_add_date, 'YYYY-MM-DD HH:mi:ss')`), 'so_add_date']
@@ -25,7 +28,9 @@ class PickingListController {
                     as: 'detail_sales_order',
                     attributes: [
                         'sod_oid',
+                        'sod_pt_id',
                         [Sequelize.literal(`"detail_sales_order->detail_product"."pt_en_id"`), 'sod_pt_en_id'],
+                        [Sequelize.literal(`"detail_sales_order->detail_product"."pt_code"`), 'sod_pt_code'],
                         [Sequelize.literal(`"detail_sales_order->detail_product->data_entity"."en_desc"`), 'sod_en_desc'],
                         [Sequelize.literal(`"detail_sales_order->detail_product"."pt_desc1"`), 'sod_pt_desc1'],
                         [Sequelize.literal(`"detail_sales_order->detail_sublocation"."pcklsd_sod_qty"`), 'sod_qty'],
@@ -75,34 +80,80 @@ class PickingListController {
     }
 
     async putProductIntoGITSublocation (req, res) {
+        let transaction = await sequelize.transaction()
+
         try {
-            let {usernama} = Auth(req.headers['authorization'])
+            // get data user
+            let {usernama} = await Auth(req.headers['authorization'])
+
+            // get start sublocation & sublocation destination
+            let {locs_id: sublocationDestination} = await LocsMstr.findOne({attributes: ['locs_id'], where: {locs_name: req.body.sublocationDestination}})
 
             // get qty from original sublocation
-            let {invcd_oid: oidStartSublocation, invcd_qty: qtyStartSublocation} = await InvcdDet.findOne({attributes: ['invcd_oid', 'invcd_qty'], where: {invcd_pt_id: req.body.ptId, invcd_locs_id: req.body.startingSublocation}})
+            let {invcd_oid: oidStartSublocation, invcd_qty: qtyStartSublocation} = await InvcdDet.findOne({
+                attributes: ['invcd_oid', 'invcd_qty'], 
+                where: {
+                    invcd_pt_id: req.body.ptId, 
+                    invcd_locs_id: {
+                        [Op.eq]: Sequelize.literal(`(SELECT locs_id FROM public.locs_mstr WHERE locs_name = '${req.body.startingSublocation}')`)
+                    }
+                }
+            })
+
+            // update qty from original sublocation
+            let updateQtyOriginalSublocation = InvcdDet.update({
+                invcd_qty: parseInt(qtyStartSublocation) - parseInt(req.body.qty),
+                invcd_upd_by: usernama,
+                invcd_upd_date: moment().format('YYYY-MM-DD HH:mm:ss')
+            }, {
+                where: {
+                    invcd_oid: oidStartSublocation
+                }
+            })
 
             // input data product based on git sublocation
-            let createdDataBasedGitSublocation = await InvcdDet.create({
+            let createdDataBasedGitSublocation = InvcdDet.create({
                 invcd_oid: uuidv4(),
                 invcd_en_id: req.body.enId,
                 invcd_pt_id: req.body.ptId,
                 invcd_qty: req.body.qty,
-                invcd_locs_id: req.body.locationDestination,
+                invcd_locs_id: sublocationDestination,
                 invcd_add_date: moment().format('YYYY-MM-DD HH:mm:ss'),
                 invcd_add_by: usernama
             })
 
-            let updateSodDet = await SodDet.update({
-                sod_qty_picked: req.body.qty,
+            // update table sod_det
+            let updateSodDet = SodDet.update({
+                sod_qty_picked: parseInt(req.body.qty),
                 sod_upd_by: usernama,
                 sod_upd_date: moment().format('YYYY-MM-DD HH:mm:ss')
             }, {
                 where: {
-                    // sod_oid
+                    sod_so_oid: req.body.soOid,
+                    sod_pt_id: req.body.ptId
                 }
             })
+
+            // run 3 query with Promise
+            let proccess = await Promise.all([updateQtyOriginalSublocation, createdDataBasedGitSublocation, updateSodDet])
+
+            transaction.commit()
+
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: proccess[1],
+                    error: null
+                })
         } catch (error) {
-            
+            transaction.rollback()
+
+            res.status(400)
+                .json({
+                    status: 'failed',
+                    data: null,
+                    error: error.message
+                })
         }
     }
 }
--- routes/picking_list.js
+++ routes/picking_list.js
@@ -3,6 +3,7 @@ const router = Router()
 const PickingListController = require('../app/Controller/PickingListController')
 const {authenticate} = require('../app/Middleware/middleware')
 
-router.get('/:soCode/detail', [authenticate], PickingListController.getDataSalesOrder)
+router.get('/:soCode/detail', [authenticate], PickingListController.getDataSalesOrder);
+router.post('/create', [authenticate], PickingListController.putProductIntoGITSublocation);
 
 module.exports = router
