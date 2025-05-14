e9bef6565f90770f2d6da893da70872b1e7f6369
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-05-14 11:41:20 +0700

    update controller

--- app/Controller/ScanoutController.js
+++ app/Controller/ScanoutController.js
@@ -48,7 +48,7 @@ class ScanoutController {
 
     createDetail = (req, res) => {
         sequelize.transaction(async t => {
-            let dataSerial = await InventoryService.findRegisteredSerialNumber(req.body.uniq, t);
+            let dataSerial = await InventoryService.newFindRegisteredSerialNumber(req.body.uniq, t);
 
             if (!dataSerial) {
                 return this.returnResponse(300, 'rejected', 'serial not found', null)
@@ -62,7 +62,7 @@ class ScanoutController {
                 await this.registerSerial(dataSerial, req.body.uniq, t);
             }
 
-            let newDataSerial = await InventoryService.findRegisteredSerialNumber(req.body.uniq, t);
+            let newDataSerial = await InventoryService.newFindRegisteredSerialNumber(req.body.uniq, t);
 
             await Promise.all([
                 InventoryService.scanoutSerial(newDataSerial.dataValues.invcd_oid, req.body.scanout_oid, t),
@@ -133,7 +133,7 @@ class ScanoutController {
     deleteScannedOut = (req, res) => {
         sequelize.transaction(async t => {
             let dataScannedOut = await ScanoutService.findSerialAlreadyScanned(req.params.scd_oid);
-            let dataSerial = await InventoryService.findRegisteredSerialNumber(dataScannedOut.dataValues.scd_serial, t);
+            let dataSerial = await InventoryService.newFindRegisteredSerialNumber(dataScannedOut.dataValues.scd_serial, t);
 
             if (!dataScannedOut) {
                 return this.returnResponse(300, 'rejected', 'serial not found', null)
--- app/Services/InventoryService.js
+++ app/Services/InventoryService.js
@@ -460,6 +460,48 @@ class InventoryService {
                     }
                 ]
             },
+            order: [['invcd_pt_id', 'ASC']],
+            transaction
+        })
+
+        return result;
+    }
+
+    newFindRegisteredSerialNumber = async (serialNumber, transaction) => {
+        let result = await InvcdDet.findOne({
+            attributes: [
+                'invcd_oid',
+                'invcd_dom_id',
+                'invcd_en_id',
+                'invcd_pt_id',
+                'invcd_loc_id',
+                'invcd_locs_id',
+                [Sequelize.col('"product"."pt_desc1"'), 'product_name'],
+                [Sequelize.col('"product"."pt_code"'), 'product_code'],
+                ['invcd_qrbarcode', 'uniq'],
+                ['invcd_alias_qrbarcode', 'alias_uniq'],
+                [Sequelize.literal('CAST(invcd_qty AS INTEGER)'), 'qty'],
+                ['invcd_en_id', 'entity_id'],
+                'invcd_is_booked'
+            ],
+            include: [
+                {
+                    model: PtMstr,
+                    as: 'product',
+                    attributes: []
+                }
+            ],
+            where: {
+                invcd_deleted_at: null,
+                invcd_deleted_by: null,
+                [Op.or]: [
+                    {
+                        invcd_qrbarcode: serialNumber
+                    }, {
+                        invcd_alias_qrbarcode: serialNumber
+                    }
+                ]
+            },
             order: [['invcd_qrbarcode', 'ASC']],
             transaction
         })
