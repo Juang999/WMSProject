a81503a9f8708ee0e7c4e8f066c79e292537e9f2
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-05-16 10:58:23 +0700

    update controller

--- app/Controller/PuttingController.js
+++ app/Controller/PuttingController.js
@@ -80,7 +80,7 @@ class PuttingController {
                 return this.returnResponse(300, 'rejected', `serial has been registered with another product | partnumber: ${req.body.partnumber}`, null, null)
             }
 
-            if (dataSerial && dataSerial.dataValues.qty != null) {
+            if (dataSerial && dataSerial.dataValues.invcd_locs_id != null) {
                 return this.returnResponse(300, 'rejected', 'serial has been registered into another sublocation', null, null);
             }
 
