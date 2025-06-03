10d94b5f54acf91401b1b0dbcb6eb6e245688998
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-06-03 09:48:13 +0700

    update controller

--- routes/return_product.js
+++ routes/return_product.js
@@ -1,9 +1,10 @@
-const { createReturnHeader, getAllHeader } = require('../app/Controller/ReturnController');
+const { createReturnHeader, getAllHeader, getHeaderScanOut } = require('../app/Controller/ReturnController');
 const { Router } = require('express');
 const router = Router();
 const authMiddleware = require('../app/Middleware/authenticate');
 
 router.post('/create-header', [ authMiddleware ], createReturnHeader);
 router.get('/', getAllHeader);
+router.get('/header-scanout', getHeaderScanOut);
 
 module.exports = router;
