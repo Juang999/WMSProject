2f5f8fe5e119d1bf5858b733715dbbdcc68aec85
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-06-13 15:06:46 +0700

    update controller

--- app/Services/InventoryService.js
+++ app/Services/InventoryService.js
@@ -408,6 +408,7 @@ class InventoryService {
                 ['invcd_alias_qrbarcode', 'alias_uniq'],
                 [Sequelize.literal('CAST(invcd_qty AS INTEGER)'), 'qty'],
                 ['invcd_en_id', 'entity_id'],
+                'invcd_status'
             ],
             include: [
                 {
